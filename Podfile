be0906fbc269ec92d519e560225edbc7bababe1c
Vladimir Cirkovic <wladimir.cirkovic@gmail.com>
2017-02-07 22:24:52 +0100

    work

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'GuessCity' do
+	pod 'SwiftPhoenixClient'
+end
---
+++ Pods/Target Support Files/Pods-GuessCity/Pods-GuessCity-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GuessCity : NSObject
+@end
+@implementation PodsDummy_Pods_GuessCity
+@end
